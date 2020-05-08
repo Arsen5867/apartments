0c9b5197bf37e8ecb201f3dbd9ffd278bb40c37e
valansevicha@mail.ru <valansevich@mail.ru>
2020-05-08 12:18:57 +0300

    first commit

---
+++ JavaScript/script.js
@@ -0,0 +1,16 @@
+/* document.getElementById('yr');
+document.getElementsByClassName('asdad');
+document.getElementsByTagName('br');
+document.querySelector('.ckassName');
+document.querySelectorAll('div'); */
+
+
+document.getElementById('burger').onclick = function () {
+    addMenu();
+};
+
+function addMenu() {
+    document.getElementById('menu').classList.toggle('show');
+};
+
+/** Add Slider */
